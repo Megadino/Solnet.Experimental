38763297f5cc9cc07e0a0c5bf51f83dc91f4b38d
Megadino <megacom.net@gmail.com>
2025-02-03 20:41:59 +0100

    PumpFun helper classes

---
+++ src/Solnet.Programs/PumpFun/Accounts.cs
@@ -0,0 +1,37 @@
+﻿using Solnet.Programs.Utilities;
+using System;
+
+namespace Solnet.Pumpfun
+{
+    public class Accounts
+    {
+        public partial class BondingCurve
+        {
+
+            public ulong virtualTokenReserves;
+            public ulong virtualSolReserves;
+            public ulong realTokenReserves;
+            public ulong realSolReserves;
+            public ulong tokenTokenSupply;
+            public bool complete;
+            public static BondingCurve Deserialize(ReadOnlySpan<byte> _data)
+            {
+                int offset = 8;
+                BondingCurve result = new BondingCurve();
+                result.virtualTokenReserves = _data.GetU64(offset);
+                offset += 8;
+                result.virtualSolReserves = _data.GetU64(offset);
+                offset += 8;
+                result.realTokenReserves = _data.GetU64(offset);
+                offset += 8;
+                result.realSolReserves = _data.GetU64(offset);
+                offset += 8;
+                result.tokenTokenSupply = _data.GetU64(offset);
+                offset += 8;
+                result.complete = _data.GetBool(offset);
+
+                return result;
+            }
+        }
+    }
+}
---
+++ src/Solnet.Programs/PumpFun/PDALookup.cs
@@ -0,0 +1,52 @@
+﻿using Solnet.Programs;
+using Solnet.Wallet;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solnet.Pumpfun
+{
+    public static class PDALookup
+    {
+        public static PublicKey FindGlobalPDA()
+        {
+            PublicKey.TryFindProgramAddress(new List<byte[]>()
+            {
+                Encoding.UTF8.GetBytes("global")
+            },
+            PumpfunProgram.ProgramID,
+            out PublicKey globalAddress,
+            out _);
+
+            return globalAddress;
+        }
+
+        public static PublicKey FindBondingPDA(PublicKey mintAddress)
+        {
+            PublicKey.TryFindProgramAddress(new List<byte[]>()
+            {
+                new byte[]{ 98, 111, 110, 100, 105, 110, 103, 45, 99, 117, 114, 118, 101 },
+                mintAddress.KeyBytes
+            },
+            PumpfunProgram.ProgramID,
+            out PublicKey bondingAddress,
+            out _);
+
+            return bondingAddress;
+        }
+
+        public static PublicKey FindAssociatedBondingPDA(PublicKey bondingAddress, PublicKey mintAddress)
+        {
+            PublicKey.TryFindProgramAddress(new List<byte[]>()
+            {
+                bondingAddress.KeyBytes,
+                TokenProgram.ProgramIdKey,
+                mintAddress.KeyBytes
+            },
+            AssociatedTokenAccountProgram.ProgramIdKey,
+            out PublicKey asscociated_bondingAddress,
+            out _);
+
+            return asscociated_bondingAddress;
+        }
+    }
+}
